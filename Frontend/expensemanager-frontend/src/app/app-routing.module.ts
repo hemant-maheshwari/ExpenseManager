c931a9755c64b2e7e4037d2faf1930686ef6507a
Adrian Gallant <adrian.gallant@student.fairfield.edu>
2020-11-08 19:46:20 -0500

    nav bar includes links to all components now

--- Frontend/expensemanager-frontend/src/app/app-routing.module.ts
+++ Frontend/expensemanager-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {ExpensesComponent} from './expenses/expenses.component';
+import {TransactionComponent} from './transaction/transaction.component';
+import {FriendsComponent} from './friends/friends.component';
+import {AddFriendComponent} from './add-friend/add-friend.component';
+import {AccountComponent} from './account/account.component';
+import {ActivityComponent} from './activity/activity.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'forgot-password', component: ForgotPasswordComponent}
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'expenses', component: ExpensesComponent},
+  {path: 'transaction', component: TransactionComponent},
+  {path: 'friends', component: FriendsComponent},
+  {path: 'add-friend', component: AddFriendComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'activity', component: ActivityComponent}
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- Frontend/expensemanager-frontend/src/app/app.module.ts
+++ Frontend/expensemanager-frontend/src/app/app.module.ts
@@ -7,13 +7,25 @@ import { LoginComponent} from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { routing } from './app-routing.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { FriendsComponent } from './friends/friends.component';
+import { ActivityComponent } from './activity/activity.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { AccountComponent } from './account/account.component';
+import { AddFriendComponent } from './add-friend/add-friend.component';
+import { ExpensesComponent } from './expenses/expenses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    FriendsComponent,
+    ActivityComponent,
+    TransactionComponent,
+    AccountComponent,
+    AddFriendComponent,
+    ExpensesComponent
   ],
   imports: [
     routing,
